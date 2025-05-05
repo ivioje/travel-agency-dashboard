aa66e7e0bd2d89c5e1c82283dab6f271c4957b8c
Rhoda Eboreime <rhodaivioje@gmail.com>
2025-05-05 11:34:06 +0100

    Add admin dashboard components and layout; implement user statistics and trip cards

--- app/routes/admin/admin-layout.tsx
+++ app/routes/admin/admin-layout.tsx
@@ -1,14 +1,25 @@
+import pkg from "@syncfusion/ej2-react-navigations";
+const {SidebarComponent} = pkg;
 import {Outlet} from "react-router";
+import {MobileSidebar, NavItems} from "../../../components";
+
+const AdminLayout = () => {
+    return (
+        <div className="admin-layout">
+            MobileSidebar
+            <aside className="w-full max-w-[270px] hidden lg:block">Sidebar</aside>
+            <MobileSidebar />
  
- const AdminLayout = () => {
-     return (
-         <div className="admin-layout">
-             MobileSidebar
-             <aside className="w-full max-w-[270px] hidden lg:block">Sidebar</aside>
-             <aside className="children">
-                 <Outlet />
-             </aside>
-         </div>
-     )
- }
- export default AdminLayout
+            <aside className="w-full max-w-[270px] hidden lg:block">
+                <SidebarComponent width={270} enableGestures={false}>
+                    <NavItems />
+                </SidebarComponent>
+            </aside>
+ 
+            <aside className="children">
+                <Outlet />
+            </aside>
+        </div>
+    )
+}
+export default AdminLayout
